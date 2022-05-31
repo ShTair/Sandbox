88386124ada3efe2dda555cd2fcfb77a1bea5501
ShTair <git@vsh.jp>
2022-05-31 17:29:18 +0900

    インライン化の話

---
+++ Sandbox/2022/202205/Class2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace Sandbox._2022._202205;
+
+public class Class2
+{
+    public Class2()
+    {
+        var i = -1 >>> 2;
+        var s = "hello"u8;
+        ReadOnlySpan<byte> ros = "hello"u8;
+
+        // 文字列の最適化がんばってる
+
+        Regex.EnumerateMatches("test", $".");
+
+        var input = "test";
+        foreach (var match in Regex.EnumerateMatches(input, "a"))
+        {
+            var ros2 = input.AsSpan(match.Index, match.Length);
+        }
+    }
+
+    [Fact]
+    public void Check()
+    {
+        var subset = "hello".AsSpan()[2..4];
+    }
+}
+
+static class Extensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReadOnlySpan<char> ToSpan(this ValueMatch m, string input)
+    {
+        return input.AsSpan(m.Index, m.Length);
+    }
+}
---
+++ Sandbox/2022/202205/Class3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace Sandbox._2022._202205;
+
+internal class Class3
+{
+    public Class3()
+    {
+
+        var set = new[] { 1, 2, 3 };
+        foreach (var item in set)
+        {
+
+        }
+
+
+        for (int i = 0; i < set.Length; i++)
+        {
+            var item = set[i];
+
+        }
+
+
+        var v = M1(set, 4);
+        Console.WriteLine(v);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private int M1(int[] a, int b)
+    {
+        ArgumentNullException.ThrowIfNull(a);
+        return a.Length + b;
+    }
+
+    private void ThrowException(string message)
+    {
+        throw new ArgumentException(message);
+    }
+}
+
