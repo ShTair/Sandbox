90df9663afe6e4728a8f30b1766aca1af9278275
ShTair <git@vsh.jp>
2022-07-28 13:21:08 +0900

    コンパイルした日を知りたい

---
+++ Sandbox/2022/202207/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace Sandbox._2022._202207;
+
+internal class Class2
+{
+    public Class2()
+    {
+        // アセンブリバージョンを 1.0.* みたいに設定しておく
+        // 決定論的コンパイルをOFFにしておく
+
+        var version = Assembly.GetEntryAssembly()!.GetName().Version!;
+        var buildDateTime = new DateTime(2000, 1, 1).AddDays(version.Build).AddSeconds(version.Revision * 2);
+    }
+}
